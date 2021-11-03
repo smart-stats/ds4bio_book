a72cd2538e4201810d3f9d5617b6d06faf85695c
Brian Caffo <bcaffo@gmail.com>
2021-11-03 15:54:14 -0400

    WOrked on the sql chapter

--- book/sql/sqlChapterCode2.sql
+++ book/sql/sqlChapterCode2.sql
@@ -8,3 +8,12 @@ pragma table_info(annual);
 pragma table_info(land);
 select BUYER_COUNTY, BUYER_STATE, STATE, COUNTY, year, population from population limit 5;
 select * from annual where countyfips = "NA" limit 10;
+update annual set countyfips = 05097 where BUYER_STATE = "AR" and BUYER_COUNTY = "MONTGOMERY";
+select * from annual where BUYER_STATE = "AR" and BUYER_COUNTY = "MONTGOMERY";
+select * from annual where BUYER_COUNTY = "NA";
+delete from annual where BUYER_COUNTY = "NA";
+select * from annual where BUYER_COUNTY = "NA";
+create table land_area as select Areaname, STCOU, LND110210D from land;
+alter table land_area rename column STCOU to countyfips;
+create table county_info as select * from population left join land_area using(countyfips);
+

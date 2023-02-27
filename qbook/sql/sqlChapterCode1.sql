69896bb6de9b4f0f3682effdf63a21f8ce1e6374
Brian Caffo <bcaffo@gmail.com>
2023-02-27 06:27:23 -0500

    Added a bunch of files

---
+++ qbook/sql/sqlChapterCode1.sql
@@ -0,0 +1,10 @@
+create table class(id int primary key, lname text,fname);
+insert into class values (1, "Wayne", "Bruce");
+insert into class values (2, "Jennifer", "Walters");
+.header on
+.mode column
+select * from class
+
+
+
+

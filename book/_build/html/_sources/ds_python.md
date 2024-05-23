bee1d6b1c0f592d4e9659fd3b9d8c9ba4f80749e
Brian Caffo <bcaffo@gmail.com>
2024-05-23 07:16:35 -0400

    Working on an update of the jupyter book

--- book/_build/html/_sources/ds_python.md
+++ book/_build/html/_sources/ds_python.md
@@ -1,4 +1,6 @@
-# Python background
+# Python
+
+## Python background
 
 First, you'll need a place to program python for data analysis. Python has a dizzying array of options for its use. 
 A first choice is whether you'll use python locally (installed on your computer) or in the cloud. The cloud options
--- qbook/_book/site_libs/quarto-nav/quarto-nav.js
+++ qbook/_book/site_libs/quarto-nav/quarto-nav.js
@@ -85,6 +85,17 @@ window.document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  function dashboardOffset() {
+    const dashboardNavEl = window.document.getElementById(
+      "quarto-dashboard-header"
+    );
+    if (dashboardNavEl !== null) {
+      return dashboardNavEl.clientHeight;
+    } else {
+      return 0;
+    }
+  }
+
   function updateDocumentOffsetWithoutAnimation() {
     updateDocumentOffset(false);
   }
@@ -92,7 +103,7 @@ window.document.addEventListener("DOMContentLoaded", function () {
   function updateDocumentOffset(animated) {
     // set body offset
     const topOffset = headerOffset();
-    const bodyOffset = topOffset + footerOffset();
+    const bodyOffset = topOffset + footerOffset() + dashboardOffset();
     const bodyEl = window.document.body;
     bodyEl.setAttribute("data-bs-offset", topOffset);
     bodyEl.style.paddingTop = topOffset + "px";
@@ -193,7 +204,11 @@ window.document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener(
     "hashchange",
     function (e) {
-      window.scrollTo(0, window.pageYOffset - headerOffset());
+      if (
+        getComputedStyle(document.documentElement).scrollBehavior !== "smooth"
+      ) {
+        window.scrollTo(0, window.pageYOffset - headerOffset());
+      }
     },
     false
   );
@@ -201,9 +216,9 @@ window.document.addEventListener("DOMContentLoaded", function () {
   // Observe size changed for the header
   const headerEl = window.document.querySelector("header.fixed-top");
   if (headerEl && window.ResizeObserver) {
-    const observer = new window.ResizeObserver(
-      updateDocumentOffsetWithoutAnimation
-    );
+    const observer = new window.ResizeObserver(() => {
+      setTimeout(updateDocumentOffsetWithoutAnimation, 0);
+    });
     observer.observe(headerEl, {
       attributes: true,
       childList: true,
@@ -222,6 +237,7 @@ window.document.addEventListener("DOMContentLoaded", function () {
     const links = window.document.querySelectorAll("a");
     for (let i = 0; i < links.length; i++) {
       if (links[i].href) {
+        links[i].dataset.originalHref = links[i].href;
         links[i].href = links[i].href.replace(/\/index\.html/, "/");
       }
     }
@@ -229,7 +245,7 @@ window.document.addEventListener("DOMContentLoaded", function () {
     // Fixup any sharing links that require urls
     // Append url to any sharing urls
     const sharingLinks = window.document.querySelectorAll(
-      "a.sidebar-tools-main-item"
+      "a.sidebar-tools-main-item, a.quarto-navigation-tool, a.quarto-navbar-tools, a.quarto-navbar-tools-item"
     );
     for (let i = 0; i < sharingLinks.length; i++) {
       const sharingLink = sharingLinks[i];
